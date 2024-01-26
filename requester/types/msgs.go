f826560e8f4df1a9b461b498fee8fc9ae13da160
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-01-26 15:37:21 +0700

    cleanup

--- requester/types/msgs.go
+++ requester/types/msgs.go
@@ -12,16 +12,3 @@ type FailResponse interface {
 	Task
 	Error() string
 }
-
-//
-//// type EventHandler interface {
-//// 	AfterTransactionSuccess(hash string, requestId uint64)
-//// 	AfterTransactionFailed(hash string, code uint64, reason string) (retry bool)
-//// 	AfterRequestResolve(requestId uint64, status uint8, result []byte) (retry bool)
-//// }
-//
-//// Channel receive request
-//// Pick a sender + create message/transaction -> broadcast
-//// Wait for block confirm -> If fail what we will do?
-//// Got request id wait resolve -> If fail what we will do?
-//// Got result then wait on signature -> If fail what we will do?
