7773b70bc8ddc89c3e81802010f4913f5a99436d
Warittorn Cheevachaipimol <warittorn@bandprotocol.com>
2024-01-31 14:10:18 +0700

    fix

---
+++ requester/watcher/request/watcher_test.go
@@ -0,0 +1,179 @@
+package request_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	oracletypes "github.com/bandprotocol/chain/v2/x/oracle/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"go.uber.org/mock/gomock"
+
+	client "github.com/bandprotocol/go-band-sdk/client/mock"
+	"github.com/bandprotocol/go-band-sdk/requester/watcher/request"
+	logging "github.com/bandprotocol/go-band-sdk/utils/logging/mock"
+)
+
+func TestWatcher(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	res := oracletypes.Result{
+		ClientID:       "",
+		OracleScriptID: 0,
+		Calldata:       nil,
+		AskCount:       0,
+		MinCount:       0,
+		RequestID:      0,
+		AnsCount:       0,
+		RequestTime:    0,
+		ResolveTime:    0,
+		ResolveStatus:  1,
+		Result:         nil,
+	}
+	mockClient := client.NewMockClient(ctrl)
+	mockClient.EXPECT().GetResult(gomock.Any()).Return(&res, nil).Times(1)
+
+	mockLogger := logging.NewLogger()
+
+	watcherCh := make(chan request.Task, 100)
+
+	w := request.NewWatcher(mockClient, mockLogger, 3*time.Second, 60*time.Second, watcherCh, 100, 100)
+	go w.Start()
+
+	task := request.Task{
+		RequestID: 1,
+		Msg: oracletypes.MsgRequestData{
+			OracleScriptID: 1,
+			Calldata:       []byte{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			AskCount:       16,
+			MinCount:       10,
+			ClientID:       "test",
+			FeeLimit:       sdk.NewCoins(sdk.NewCoin("uband", sdk.NewInt(1000000))),
+			PrepareGas:     0,
+			ExecuteGas:     0,
+			Sender:         "",
+		},
+	}
+	timeout := time.After(5 * time.Second)
+
+	watcherCh <- task
+
+	for {
+		select {
+		case _ = <-w.SuccessfulRequestCh():
+			return
+		case _ = <-w.FailedRequestCh():
+			t.Errorf("expected success, not failure")
+			return
+		case <-timeout:
+			t.Errorf("timed out")
+			return
+		default:
+			continue
+		}
+	}
+}
+
+func TestWatcherWithResolveFailure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	res := oracletypes.Result{
+		ClientID:       "",
+		OracleScriptID: 1,
+		Calldata:       nil,
+		AskCount:       10,
+		MinCount:       16,
+		RequestID:      1,
+		AnsCount:       16,
+		RequestTime:    1,
+		ResolveTime:    2,
+		ResolveStatus:  2,
+		Result:         nil,
+	}
+
+	mockClient := client.NewMockClient(ctrl)
+	mockClient.EXPECT().GetResult(gomock.Any()).Return(&res, nil).Times(1)
+
+	mockLogger := logging.NewLogger()
+
+	watcherCh := make(chan request.Task, 100)
+
+	w := request.NewWatcher(mockClient, mockLogger, 1*time.Second, 5*time.Second, watcherCh, 100, 100)
+	go w.Start()
+
+	msg := oracletypes.MsgRequestData{
+		OracleScriptID: 1,
+		Calldata:       []byte{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		AskCount:       16,
+		MinCount:       10,
+		ClientID:       "test",
+		FeeLimit:       sdk.NewCoins(sdk.NewCoin("uband", sdk.NewInt(1000000))),
+		PrepareGas:     0,
+		ExecuteGas:     0,
+		Sender:         "",
+	}
+	task := request.NewTask(1, 1, msg)
+	timeout := time.After(10 * time.Second)
+
+	watcherCh <- task
+
+	for {
+		select {
+		case _ = <-w.SuccessfulRequestCh():
+			t.Errorf("expected failure, not success")
+			return
+		case _ = <-w.FailedRequestCh():
+			return
+		case <-timeout:
+			t.Errorf("timed out")
+			return
+		default:
+			continue
+		}
+	}
+}
+
+func TestWatcherWithTimeout(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	mockClient := client.NewMockClient(ctrl)
+	mockClient.EXPECT().GetResult(gomock.Any()).Return(nil, fmt.Errorf("error")).AnyTimes()
+
+	mockLogger := logging.NewLogger()
+
+	watcherCh := make(chan request.Task, 100)
+
+	w := request.NewWatcher(mockClient, mockLogger, 1*time.Second, 5*time.Second, watcherCh, 100, 100)
+	go w.Start()
+
+	msg := oracletypes.MsgRequestData{
+		OracleScriptID: 1,
+		Calldata:       []byte{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		AskCount:       16,
+		MinCount:       10,
+		ClientID:       "test",
+		FeeLimit:       sdk.NewCoins(sdk.NewCoin("uband", sdk.NewInt(1000000))),
+		PrepareGas:     0,
+		ExecuteGas:     0,
+		Sender:         "",
+	}
+	task := request.NewTask(1, 1, msg)
+	timeout := time.After(10 * time.Second)
+
+	watcherCh <- task
+
+	for {
+		select {
+		case _ = <-w.SuccessfulRequestCh():
+			t.Errorf("expected failure due to timeout")
+			return
+		case _ = <-w.FailedRequestCh():
+			return
+		case <-timeout:
+			t.Errorf("timed out")
+			return
+		default:
+			continue
+		}
+	}
+}
---
+++ utils/logging/mock/logger.go
@@ -0,0 +1,35 @@
+package mock
+
+import (
+	"github.com/bandprotocol/go-band-sdk/utils/logging"
+)
+
+var (
+	_ logging.Logger = &Logger{}
+)
+
+type Logger struct{}
+
+func NewLogger() *Logger {
+	return &Logger{}
+}
+
+func (l *Logger) Debug(topic string, format string, args ...interface{}) {
+	return
+}
+
+func (l *Logger) Info(topic string, format string, args ...interface{}) {
+	return
+}
+
+func (l *Logger) Warning(topic string, format string, args ...interface{}) {
+	return
+}
+
+func (l *Logger) Error(topic string, format string, args ...interface{}) {
+	return
+}
+
+func (l *Logger) Critical(topic string, format string, args ...interface{}) {
+	return
+}
